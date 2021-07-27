f7c6b918ab3e529ce7db41b65c13de4831da487e
Riot-X <76699273+Th30utlaw@users.noreply.github.com>
2021-07-27 22:13:18 +0530

    Update extras.py

--- tg_bot/modules/extras.py
+++ tg_bot/modules/extras.py
@@ -205,7 +205,7 @@ TOSS = (
 @run_async
 def roll(bot: Bot, update: Update):
     update.message.reply_text(random.choice(range(1, 7)))
-    bot.send_dice()
+    bot.sendDice()
 
 def toss(bot: Bot, update: Update):
     update.message.reply_text(random.choice(TOSS))
