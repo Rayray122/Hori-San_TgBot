d75b9ffd2afd609dee4bd7ed3ef7c93c4fc6e171
Riot-X <76699273+Th30utlaw@users.noreply.github.com>
2021-07-28 16:27:00 +0530

    Update extras.py

--- tg_bot/modules/extras.py
+++ tg_bot/modules/extras.py
@@ -200,7 +200,7 @@ def toss(bot: Bot, update: Update):
 @run_async
 def abuse(bot: Bot, update: Update):
     # reply to correct message
-    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
     reply_text(random.choice(ABUSE_STRINGS))
 	
 @run_async
